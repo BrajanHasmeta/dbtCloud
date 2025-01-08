a051317fe292a1cd1ac360d2a04a7af97634fe77
brajan.hasmeta@fti.edu.al <brajan.hasmeta@fti.edu.al>
2025-01-08 16:58:30 +0000

    b

--- models/example/transformDBT_Table.sql
+++ models/example/transformDBT_Table.sql
@@ -1,8 +1,8 @@
+{{ config(materialized='table') }}
 with
     updated_data as (
         select id, 'BRAJAN' as description  -- Set the description column to NULL
         from {{ source("test", "DBT") }}  -- Reference the source table
     )
-
 select *
 from updated_data
--- models/example/updateDBT.sql
+++
@@ -1,4 +0,0 @@
-{% macro update_description() %}
-UPDATE {{ source('TEST_DB', 'DBT') }}
-SET description = 'updated' WHERE id=1;
-{% endmacro %}
